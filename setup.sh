05f695fb4cc345d2ed329c52d8ab2671d8e34cac
Lei Shi <me@leishi.io>
2019-09-12 14:06:12 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+sudo apt update
+
+sudo apt install -y git vim build-essential zsh htop nginx
+
+# Switch to zsh and set up oh-my-zsh
+sudo chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Setup pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshenv
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshenv
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
+source ~/.zshenv
+
+# Setup pyenv virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshenv

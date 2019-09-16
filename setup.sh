91c56893681b1553f5ca9418dbcce63095d5d58f
Lei Shi <me@leishi.io>
2019-09-17 02:31:49 +0800

    Add zshrc and zshenv

--- setup.sh
+++ setup.sh
@@ -10,11 +10,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Setup pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshenv
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshenv
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshenv
 source ~/.zshenv
 
 # Setup pyenv virtualenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshenv

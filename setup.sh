88a738acca344c225287d01a0476dd9650b2b33d
Lei Shi <me@leishi.io>
2019-09-18 15:26:42 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
 # Switch to zsh and set up oh-my-zsh
-chsh -s $(which zsh)
+sudo -u $(whoami) -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Setup pyenv
